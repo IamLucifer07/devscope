3f93117162b503e1125f6f8541a1c2796c14ffaf
Gaurav Chaulagain <talktogaurav07@gmail.com>
2026-02-20 22:45:59 +0545

    fetch all repos

--- src/services/githubApi.ts
+++ src/services/githubApi.ts
@@ -9,7 +9,27 @@ export const fetchUser = async (username: string) => {
     return data;
 };
 
+// -------- fetch one page only --------
 export const fetchRepos = async (username: string) => {
     const { data } = await githubApi.get(`/users/${username}/repos`);
     return data;
 };
+
+// fetch all repos with pagination
+// export const fetchRepos = async (username: string) => {
+//     let page = 1;
+//     let allRepos: any[] = [];
+
+//     while (true) {
+//         const { data } = await githubApi.get(
+//             `/users/${username}/repos?per_page=100&page=${page}`
+//         );
+
+//         if (data.length === 0) break;
+
+//         allRepos = [...allRepos, ...data];
+//         page++;
+//     }
+
+//     return allRepos;
+// };
