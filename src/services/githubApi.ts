dd51d9929640ad51036fc1dfb3de2be2ce5297a4
Gaurav Chaulagain <talktogaurav07@gmail.com>
2026-02-22 21:28:50 +0545

    github api

--- src/services/githubApi.ts
+++ src/services/githubApi.ts
@@ -1,35 +1,25 @@
 import axios from "axios";
+import { GithubRepo, GithubUser } from "@/types/github";
 
 const githubApi = axios.create({
-    baseURL: "https://api.github.com",
+    baseURL: "/api/github",
+    headers: {
+        Accept: "application/vnd.github.v3+json",
+    },
 });
 
 export const fetchUser = async (username: string) => {
-    const { data } = await githubApi.get(`/users/${username}`);
+    const { data } = await githubApi.get<GithubUser>(
+        `/user?username=${encodeURIComponent(username)}`
+    );
     return data;
 };
 
-// -------- fetch one page only --------
+export default githubApi;
+
 export const fetchRepos = async (username: string) => {
-    const { data } = await githubApi.get(`/users/${username}/repos`);
+    const { data } = await githubApi.get<GithubRepo[]>(
+        `/repos?username=${encodeURIComponent(username)}`
+    );
     return data;
 };
-
-// fetch all repos with pagination
-// export const fetchRepos = async (username: string) => {
-//     let page = 1;
-//     let allRepos: any[] = [];
-
-//     while (true) {
-//         const { data } = await githubApi.get(
-//             `/users/${username}/repos?per_page=100&page=${page}`
-//         );
-
-//         if (data.length === 0) break;
-
-//         allRepos = [...allRepos, ...data];
-//         page++;
-//     }
-
-//     return allRepos;
-// };
